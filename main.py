8cf729b26ffbed33ad169379e35bbabc0b00f2f7
Sebastien <sebastienboehme@gmail.com>
2021-07-05 06:50:01 -0700

    trying to set up an initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import time
+import tkinter
+fs = 44100  # Sample rate
+
+class gui:
+    def __init__(self):
+        root = tkinter.Tk
+
+
+myrecording = sd.rec(frames=(44100 * 5), blocking=True, samplerate=fs, channels=2)
+sd.stop()
+write('output.wav', fs, myrecording)  # Save as WAV file
