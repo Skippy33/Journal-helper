4450aebf831895339eafe9bcf0388f881732b717
Sebastien <sebastienboehme@gmail.com>
2021-07-06 15:40:17 -0700

    records audio, added pause/reset buttons (but they dont do anything yet)

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import pyaudio
 import wave
-import time
 import tkinter
 
 #making a class for stuff related to recording
 class Recording:
+
+
+    # make a class for all the things for recording controls
+    class Recordcontrols():
+        # initialize
+        def __init__(self, window):
+            self.window = window
+            pausephoto = tkinter.PhotoImage(file=(r"C:\Users\Sebastien\PycharmProjects\Journaling\assets\pause.png"))
+            pausephoto = pausephoto.subsample(3, 3)
+            self.pausebutton = tkinter.Button(self.window, image=pausephoto)
+            self.pausebutton.image = pausephoto
+            stopphoto = tkinter.PhotoImage(file=(r"C:\Users\Sebastien\PycharmProjects\Journaling\assets\stop.png"))
+            stopphoto = stopphoto.subsample(3, 3)
+            self.stopbutton = tkinter.Button(self.window, image=stopphoto)
+            self.stopbutton.image = stopphoto
+
+        def StartRecording(self):
+            self.pausebutton.pack(side="left")
+            self.stopbutton.pack(side="right")
+            self.window.geometry("400x200")
+            tkinter.mainloop()
+
+        # destroys buttons at end of recording
+        def EndRecording(self):
+            self.pausebutton.destroy()
+            self.stopbutton.destroy()
+
+
     #initialize the variables
     def __init__(self, recordbutton, window):
         #self.window takes the place of tk.Tk()
@@ -13,15 +40,18 @@ class Recording:
         self.recordbutton = recordbutton
         #audio recording variable
         self.audio = pyaudio.PyAudio()
+        self.recordcontrols = self.Recordcontrols(self.window)
         #list of frames
         self.frames = []
 
     #starts recording
     def startrecording(self):
-        print("started recording")
         #starts the recording
         self.stream = self.audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
         #go back to start
+        self.recordcontrols.pausebutton.pack(side="left")
+        self.recordcontrols.stopbutton.pack(side="right")
+        self.window.geometry("400x200")
         self.recordswitch()
 
     #stops recording
@@ -47,6 +77,8 @@ class Recording:
         #clears frames list for any more recordings
         self.frames = []
 
+        print("stopped")
+
     #switches the recording
     def recordswitch(self, from_button=False):
         #update the window
@@ -60,12 +92,14 @@ class Recording:
             #start recording
             self.startrecording()
 
+
         #elif the text is "stop[ recording" and the press comes from the button
         elif self.recordbutton["text"] == "stop recording" and from_button:
+            print("stopped")
             #update the text
             self.recordbutton["text"] = "start recording"
             self.recordbutton.update()
-            #stop the recording
+            self.recordcontrols.EndRecording()
             self.stoprecording()
 
         #elif it is currently recording and not gettwing a call from the button
@@ -86,13 +120,13 @@ def Main():
     window.geometry("200x100")
     #make a record button
     recordbutton = tkinter.Button(window, text="start recording", padx=10, pady=15)
-    recordbutton.pack()
+    recordbutton.pack(side="top")
 
     #start a recording class
     record = Recording(recordbutton, window)
     #configure the recordbutton
     recordbutton.configure(command=lambda : record.recordswitch(from_button=True))
-    recordbutton.pack()
+    recordbutton.pack(side="top")
     #mainloop
     window.mainloop()
 
@@ -101,3 +135,4 @@ if __name__ == '__main__':
     Main()
 
 #make a way to transcribe, maybe have a popup to tell person that they are recording, or have pause/continue buttons
+#needs to end/pause/restart recording
